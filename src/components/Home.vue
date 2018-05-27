282a3724ca30da6cdc8092b649af78097628284c
flamezaxaou10 <flamezaxaou1@gmail.com>
2018-05-28 02:38:50 +0700

    55

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'Input',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    ...mapActions([
+      'inputProduct'
+    ]),
+    addProduct () {
+      this.inputProduct(this.product)
+      this.product.name = ''
+      this.product.price = ''
+      this.product.count = ''
+      this.product.img = ''
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
